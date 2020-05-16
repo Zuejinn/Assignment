829fbbf7e57025cb86e63c5624eef327d719e5ca
Zuejinn <32529254+Zuejinn@users.noreply.github.com>
2020-05-16 11:55:19 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/utas/mekvall/assignment/CustomerList.java
@@ -0,0 +1,14 @@
+package au.edu.utas.mekvall.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_list);
+    }
+}
---
+++ app/src/main/java/au/edu/utas/mekvall/assignment/RaffleList.java
@@ -0,0 +1,14 @@
+package au.edu.utas.mekvall.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RaffleList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raffle_list);
+    }
+}
